7a7e7705aa72cf8d5e431bf42cd67d51e237cefb
Akas Patranobis <70289805+akaspatranobis@users.noreply.github.com>
2024-09-29 10:25:53 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "akhotstarbucket" # Replace with your actual S3 bucket name
+    bucket = "akhotstarbucket-akas" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
